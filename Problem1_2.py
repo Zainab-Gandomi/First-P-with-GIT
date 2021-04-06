1046a5046312bb15f7dda386bb5addc554caf6bd
Zainab-Gandomi <79536268+Zainab-Gandomi@users.noreply.github.com>
2021-04-06 12:47:57 +0100

    Add files via upload

---
+++ Problem1_2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 12 11:32:55 2021
+
+@author: zainab
+"""
+
+#%%
+def problem1_2(x,y):
+    s = x + y
+    p = x * y
+    print (s)
+    print (p)
+        
+
+#%%
