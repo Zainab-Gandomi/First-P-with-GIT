50b41ec56d5cd794547ed81cf58b0d3f3fb12208
Zainab-Gandomi <79536268+Zainab-Gandomi@users.noreply.github.com>
2021-04-06 12:50:53 +0100

    Add files via upload

---
+++ problem2_1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 13 19:22:57 2021
+
+@author: zainab
+"""
+
+def problem2_1():
+    lis = list(range(20,30))
+    print(lis[3])
+    print(lis)
+    for i in lis:
+        print(i, end =" ")
+        
+
+
+
+
+
+
+#%%
